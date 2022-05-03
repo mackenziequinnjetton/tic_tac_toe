4bb1535fe6c2d9f2b837ebafdd7062471925467d
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-02 21:30:19 -0600

    Merge test_display_board into test_make_move

--- Tests/game_test.rb
+++ Tests/game_test.rb
@@ -31,30 +31,33 @@ class GameTest < Minitest::Test
     assert_equal(:game_concluded, @test_game.play)
   end
 
-  def test_make_move
-    input_to_test_io('a3')
-
-    @test_game.make_move
-
-    assert_equal([%w[- - -], %w[- - -], %w[X - -]], @test_game.moves)
-
-    reset_test_io
-  end
-
   def exp_board
     "   a     b     c\n" \
     "      |     |\n" \
     "1  -  |  -  |  -\n" \
     " _____|_____|_____\n" \
     "      |     |\n" \
-    "2  -  |  -  |  O\n" \
+    "2  -  |  -  |  -\n" \
     " _____|_____|_____\n" \
     "      |     |\n" \
-    "3  -  |  -  |  -\n" \
+    "3  X  |  -  |  -\n" \
     "      |     |\n"
   end
 
+  def test_make_move
+    input_to_test_io('a3')
+
+    @test_game.make_move
+    @test_game.update_board
+
+    assert_equal([%w[- - -], %w[- - -], %w[X - -]], @test_game.moves)
+    assert_equal(exp_board, @test_game.board)
+
+    # reset_test_io
+  end
+
   def test_display_board
+    skip 'Not necessary'
     @test_game.current_player = 2
 
     input_to_test_io('c2')

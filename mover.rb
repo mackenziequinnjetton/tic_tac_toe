87b73485e9f994bf9a77f87993a193dd0bee3bf2
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-04 07:54:05 -0600

    Extract section of Mover.make_move to Mover.get_move

--- mover.rb
+++ mover.rb
@@ -3,8 +3,7 @@ class Mover
 
   def make_move(current_player)
     loop do
-      puts "Player #{current_player}, please enter your move (ex: b2)"
-      move = gets.chomp.downcase
+      move = get_move
 
       unless move.match(/^[a-c][1-3]$/)
         puts 'Your move was not recognized.'
@@ -24,5 +23,12 @@ class Mover
       # moves[move_y][move_x] = (current_player == 1 ? 'X' : 'O')
       # break
     end
+  end
+
+  private
+
+  def get_move
+    puts "Player #{current_player}, please enter your move (ex: b2)"
+    gets.chomp.downcase
   end
 end

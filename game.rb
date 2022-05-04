0bfeaaa28f3c791656f92ab306c2f0c3b2892fce
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-04 07:22:31 -0600

    Copy make_move method to Mover class

--- game.rb
+++ game.rb
@@ -1,4 +1,4 @@
-require_relative 'board'
+require_relative 'mover'
 
 class Game
   private
--- mover.rb
+++ mover.rb
@@ -1,2 +1,24 @@
 class Mover
+  def make_move
+    loop do
+      puts "Player #{current_player}, please enter your move (ex: b2)"
+      move = gets.chomp.downcase
+
+      unless move.match(/^[a-c][1-3]$/)
+        puts 'Your move was not recognized.'
+        next
+      end
+
+      move_parts = move.split('')
+
+      move_y_hash = Hash['1' => :row1, '2' => :row2, '3' => :row3]
+      move_x_hash = Hash['a' => :col_a, 'b' => :col_b, 'c' => :col_c]
+
+      move_y = move_y_hash[move_parts[1]]
+      move_x = move_x_hash[move_parts[0]]
+
+      moves[move_y][move_x] = (current_player == 1 ? 'X' : 'O')
+      break
+    end
+  end
 end

00a74ae3026f7f8753c49bc3fd6f2ac28a71ab3c
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-04 07:55:04 -0600

    Create mover_test.rb

---
+++ Tests/mover_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+class MoverTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end

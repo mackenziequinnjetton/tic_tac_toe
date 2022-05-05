c70d598253b52caf0574bca5c9df615f8c99bcc7
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-05 01:08:07 -0600

    Clear Board and add include statement

--- board.rb
+++ board.rb
@@ -1,19 +1,9 @@
+require 'enumerable'
+
 class Board
-  attr_accessor :moves
-  attr_reader :layout
+  include Enumerable
 
   def initialize
-    # TODO: Move 2D array somehow, look at book for details
-    @moves = [%w[- - -], %w[- - -], %w[- - -]]
-    @layout = "   a     b     c\n" \
-              "      |     |\n" \
-              "1  -  |  -  |  -\n" \
-              " _____|_____|_____\n" \
-              "      |     |\n" \
-              "2  -  |  -  |  -\n" \
-              " _____|_____|_____\n" \
-              "      |     |\n" \
-              "3  -  |  -  |  -\n" \
-              "      |     |"
+
   end
 end

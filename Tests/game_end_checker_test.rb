25f274c8241713c2aabeaa2967bf1bb634ccb475
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-09 23:43:20 -0600

    Write incomplete GameEndCheckerTest tests

--- Tests/game_end_checker_test.rb
+++ Tests/game_end_checker_test.rb
@@ -4,14 +4,26 @@ require 'minitest/autorun'
 
 class GameEndCheckerTest < Minitest::Test
   def setup
-    # Do nothing
+    @test_game_end_checker = GameEndChecker.new
   end
 
   def teardown
     # Do nothing
   end
 
-  def test
-    skip 'Not implemented'
+  def test_winner_true
+    assert(@test_game_end_checker.winner?)
+  end
+
+  def test_winner_false
+    refute(@test_game_end_checker.winner?)
+  end
+
+  def test_draw_true
+    assert(@test_game_end_checker.draw?)
+  end
+
+  def test_draw_false
+    refute(@test_game_end_checker.draw?)
   end
 end

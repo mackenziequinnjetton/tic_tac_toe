caef1b6a7c5abb92989013c817af444e793a5589
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-09 23:08:02 -0600

    Create GameEndCheckerTest

---
+++ Tests/game_end_checker_test.rb
@@ -0,0 +1,17 @@
+require '.game_end_checker'
+
+require 'minitest/autorun'
+
+class GameEndCheckerTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end

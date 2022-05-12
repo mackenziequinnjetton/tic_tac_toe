c7542cd4715a35bbb3ad207f5dfbfbb6fade243c
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-11 23:28:31 -0600

    Create board_row_test.rb

---
+++ Tests/board_row_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+class BoardRowTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end

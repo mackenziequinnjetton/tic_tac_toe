0f59207f3d88cbeb1221fea19f8b0d09f9d2cf0b
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-12 05:56:54 -0600

    Work on WinPatternsTest tests

--- Tests/win_patterns_test.rb
+++ Tests/win_patterns_test.rb
@@ -1,8 +1,12 @@
+require './board'
+require './win_patterns'
+
 require 'minitest/autorun'
 
 class WinPatternsTest < Minitest::Test
   def setup
     @test_win_patterns = WinPatterns.new
+    @test_board = Board.new
   end
 
   def teardown
@@ -10,8 +14,128 @@ class WinPatternsTest < Minitest::Test
   end
 
   def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
 
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
 
-    assert
+  def test_win_pattern1_false
+    @test_board.row3.col_c = 'X'
+
+    refute(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
+  end
+
+  def test_win_pattern1_true
+    @test_board.row1.col_a = 'X'
+    @test_board.row2.col_b = 'X'
+    @test_board.row3.col_c = 'X'
+
+    assert(@test_win_patterns.win_pattern1(board: @test_board, str: 'X'))
   end
 end

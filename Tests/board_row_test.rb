1ac7848c92dd6e7d635084f3e4191966c7a5b056
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-11 23:31:30 -0600

    Create BoardRowTest.test_implements_each

--- Tests/board_row_test.rb
+++ Tests/board_row_test.rb
@@ -1,15 +1,17 @@
+require './board_row'
+
 require 'minitest/autorun'
 
 class BoardRowTest < Minitest::Test
   def setup
-    # Do nothing
+    @test_board_row = BoardRow.new
   end
 
   def teardown
     # Do nothing
   end
 
-  def test
-    skip 'Not implemented'
+  def test_implements_each
+    assert_respond_to(@test_board_row, :each)
   end
 end

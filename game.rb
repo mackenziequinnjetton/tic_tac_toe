db7b6279485e3500567e07d65a7dbec9985d9543
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-05-16 23:22:17 -0600

    Refactor Game and GameEndChecker methods

--- game.rb
+++ game.rb
@@ -16,8 +16,32 @@ class Game
   end
 
   def play
-    result = play_helper
+    print_result(play_helper)
+  end
 
+  private
+
+  def play_helper
+    puts board.display
+
+    game_loop
+  end
+
+  def game_loop
+    loop do
+      move_symbols = mover.make_move(current_player)
+
+      board.store_move(hash: move_symbols, current_player: current_player)
+
+      game_end_result = game_end_checker.check_game_end(board: board, current_player: current_player)
+
+      return game_end_result if %i[winner_p1 winner_p2 draw].include? game_end_result
+
+      switch_player
+    end
+  end
+
+  def print_result(result)
     case result
     when :winner_p1
       puts 'Player 1 wins!'
@@ -30,58 +54,12 @@ class Game
     result
   end
 
-  private
-
-  def play_helper
-    winner_p1 = false
-    winner_p2 = false
-    draw = false
-
-    puts board.display
-
-    loop do
-      move_symbols = mover.make_move(current_player)
-
-      board.store_move(hash: move_symbols, current_player: current_player)
-
-      if game_end_checker.winner?(board: board, current_player: current_player)
-        if current_player == 1
-          winner_p1 = true
-        else
-          winner_p2 = true
-        end
-        break
-      elsif game_end_checker.draw?(board)
-        draw = true
-        break
-      end
-      switch_player
-    end
-
-    return :winner_p1 if winner_p1
-    return :winner_p2 if winner_p2
-
-    :draw if draw
-  end
-
-  # def update_board
-  #   @board = default_board
-  # end
-
-  # Board = Struct.new(:row1, :row2, :row3, keyword_init: true)
-  # BoardRow = Struct.new(:col_a, :col_b, :col_c, keyword_init: true)
-
   def moveify
     Board.new(
       row1: BoardRow.new(col_a: '-', col_b: '-', col_c: '-'),
       row2: BoardRow.new(col_a: '-', col_b: '-', col_c: '-'),
       row3: BoardRow.new(col_a: '-', col_b: '-', col_c: '-')
     )
-    # board.map { |board_row| board_row.map { '-' } }
-
-    # moves = [%w[- - -], %w[- - -], %w[- - -]]
-
-    # moves.map { |row| BoardRow.new(row[0], row[1], row[2]) }
   end
 
   def switch_player
--- game_end_checker.rb
+++ game_end_checker.rb
@@ -11,6 +11,16 @@ class GameEndChecker
     @win_patterns = WinPatterns.new
   end
 
+  def check_game_end(board:, current_player:)
+    if winner?(board: board, current_player: current_player)
+      current_player == 1 ? :winner_p1 : :winner_p2
+    elsif draw?(board)
+      :draw
+    end
+  end
+
+  private
+
   def winner?(board:, current_player:)
     win_pattern_array = win_pattern_array(board: board, str: current_player_string(current_player))
 
@@ -23,8 +33,6 @@ class GameEndChecker
 
     false
   end
-
-  private
 
   def current_player_string(current_player)
     current_player == 1 ? 'X' : 'O'

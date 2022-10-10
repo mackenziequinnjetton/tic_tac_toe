97ea8a20fa83661187f30c1d13d95e58526ee042
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-10-10 13:01:29 -0600

    Add algorithm for selection computer opponent move
    * Add ComputerOpponent.get_move algorithm to select computer move in first available spot
    * Add ComputerOpponentTest tests for ComputerOpponent.get_move
    * Comment out BoardTest test that may be used later

--- lib/computer_opponent.rb
+++ lib/computer_opponent.rb
@@ -1,8 +1,19 @@
 class ComputerOpponent
   def initialize; end
 
-  def get_move(current_player)
-    'a1'
+  def get_move(*, board:)
+    rows = %i[row1 row2 row3]
+    cols = %i[col_a col_b col_c]
+    row_methods = [board.method(:row1), board.method(:row2), board.method(:row3)]
+
+    row_methods.each_with_index do |row_method, row_idx|
+      board_row_methods = [row_method.call.method(:col_a), row_method.call.method(:col_b),
+                           row_method.call.method(:col_c)]
+
+      board_row_methods.each_with_index do |col_method, col_idx|
+        return { row: rows[row_idx], col: cols[col_idx] } if col_method.call == '-'
+      end
+    end
   end
 
   def incomplete_message
--- spec/computer_opponent_test.rb
+++ spec/computer_opponent_test.rb
@@ -1,3 +1,4 @@
+require 'board'
 require 'computer_opponent'
 
 require 'minitest/autorun'
@@ -7,10 +8,26 @@ class ComputerOpponentTest < Minitest::Test
     @test_comp_player = ComputerOpponent.new
   end
 
-  def test_computer_player_top_left_move
-    current_player = 1
+  def test_get_move_empty_board
+    exp = { row: :row1, col: :col_a }
+    act = @test_comp_player.get_move(board: Board.new)
 
-    assert_equal('a1', @test_comp_player.get_move(current_player: 1))
+    assert_equal(exp, act)
+  end
+
+  def test_get_move_a1_taken
+    exp = { row: :row1, col: :col_b }
+    act = @test_comp_player.get_move(board: Board.new(row1: BoardRow.new(col_a: 'X')))
+
+    assert_equal(exp, act)
+  end
+
+  def test_get_move_through_c1_taken
+    exp = { row: :row2, col: :col_a }
+    act = @test_comp_player.get_move(board: Board.new(row1:
+      BoardRow.new(col_a: 'X', col_b: 'X', col_c: 'X')))
+
+    assert_equal(exp, act)
   end
 
   def test_incomplete_message
